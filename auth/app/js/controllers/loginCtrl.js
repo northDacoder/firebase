4042a8db56d89ca1f014498650b795c8913a47ba
YashYash <yash.saxena1217@gmail.com>
2014-04-11 18:15:03 -0700

    almost working

--- auth/app/js/controllers/loginCtrl.js
+++ auth/app/js/controllers/loginCtrl.js
@@ -1,8 +1,8 @@
 'use strict';
 
-app.controller('loginCtrl', function($scope, loginService){
+app.controller('loginCtrl', function($scope, loginService, $http){
 	$scope.msgtxt='';
 	$scope.login = function(user) {
-		loginService.login(user, $scope); // call login service
-	}
+		loginService.login(user, $scope); // call login service 
+	};
 });
--- auth/app/js/services/loginService.js
+++ auth/app/js/services/loginService.js
@@ -3,7 +3,14 @@
 app.factory('loginService', function($http){
 	return {
 		login:function(user, $scope){
-			var $promise=$http.post('data/user.php', user); // send data to user.php
+			// var $promise=$http.post('data/user.php', user); // send data to user.php
+			var $promise = new Firebase('https://angularauth.firebaseio.com/');
+			var email = $('#exampleInputUser1').val();
+			var pass = $('#exampleInputPassword1').val();
+			console.log(email + "," + pass);
+			$promise.push({email:email, pass:pass});
+			$('#messageInput').val('');
+			console.log(email + "," + pass); 
 			$promise.then(function(msg){
 				if (msg.data=='success') {
 					$scope.msgtxt='Correct information';
@@ -14,3 +21,4 @@ app.factory('loginService', function($http){
 		}
 	}
 });
+
